4b8724a42bcd6c14c067bc90a710214a4869776d
Piyush Golan <92436502+flywich21@users.noreply.github.com>
2023-09-03 16:41:18 +0530

    Add files via upload
     I've corrected the assignment of the audio_playing variable by using audio_playing = False and audio_playing = True where appropriate within the command_thread. This will ensure that the global audio_playing variable is updated correctly based on the pause and resume commands.

--- fsociety.py
+++ fsociety.py
@@ -1,85 +1,139 @@
-import yt_dlp as ydl
+import vlc
+import subprocess
+import sys
 import os
+import yt_dlp as ydl
 import time
-from audio_controller import AudioController
+import threading
+from colorama import init, Fore
 
-bot = 'Dara- '
+global audio_playing
+def clear():
+    return os.system('cls' if os.name == 'nt' else 'clear')
+def format_time(seconds):
+    # convert seconds to minutes and seconds
+    minutes, seconds = divmod(seconds, 60)
+    return f"{int(minutes):02d}:{int(seconds):02d}"
 
-def clear_terminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
+# specify the path to the directory containing libvlc.dll
+try:
+    VLC_DIRECTORY = "C:\\Program Files\\VideoLAN\\VLC"
+    # add the VLC directory to the system's PATH environment variable
+    os.environ["PATH"] += os.pathsep + VLC_DIRECTORY
+except:
+    print("Vlc's libvlc.dll not found error")
+    exit(0)
 
-def get_audio_url(query):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,
-        'extractaudio': True,
-        'audioformat': 'mp3',
-        'default_search': 'auto',
-    }
+def playback_thread(player, youtube_url):
+    player.play()
+    is_paused = False
+    last_print_time = 0  # initialize the last time printed
 
-    with ydl.YoutubeDL(ydl_opts) as ydl_instance:
-        info_dict = ydl_instance.extract_info(query, download=False)
-        if 'entries' in info_dict:
-            video_url = info_dict['entries'][0]['url']
+    while True:
+        # query the current playback time and duration
+        current_time = player.get_time() / 1000  # convert milliseconds to seconds
+        total_time = player.get_length() / 1000  # convert milliseconds to seconds
+
+        # check if a second has passed since the last print
+        current_print_time = int(current_time)
+        if current_print_time != last_print_time:
+            # format and print the current playback time and total duration in "mm:ss" format
+            current_time_str = format_time(current_time)
+            total_time_str = format_time(total_time)
+            print(Fore.GREEN + f"Playing {current_time_str}/{total_time_str}", end="\r")
+            last_print_time = current_print_time
+
+        time.sleep(1)
+
+def command_thread(player):
+    global audio_playing
+    while True:
+        print(f"\nPress & enter {Fore.BLUE +'Pause'} | {Fore.GREEN +'Resume'} | {Fore.RED +'Stop'} | {Fore.GREEN +'Restart'} | {Fore.RED +'Quit'}")
+        command = input(': ').strip().lower()
+        if command in ['pause', '1']:
+            if audio_playing:
+                player.set_pause(1)
+                print(Fore.GREEN + "Audio Paused")
+                audio_playing = False  # Corrected assignment
+            else:
+                print(Fore.RED + "Audio is already Paused...")
+        elif command in ['resume', '2']:
+            if not audio_playing:
+                player.set_pause(0)
+                print(Fore.GREEN + "Audio Resumed")  # Resume playback
+                audio_playing = True  # Corrected assignment
+            else:
+                print(Fore.RED + "Audio is already playing...")
+        elif command in ['stop', 'q', 'quit', '3']:
+            player.stop()
+            print(Fore.RED + "Stopping...")
+            time.sleep(2)  # stop playback
+            sys.exit(0)
+        elif command in ['restart', '4']:
+            player.stop()
+            print(Fore.GREEN + "Restarting...")
+            time.sleep(2)
+            clear()
+            main()  # restart the main function
+        elif command == "":
+            print(Fore.RED +"Input Seems Empty")
         else:
-            video_url = info_dict['url']
-
-    return video_url
+            print(Fore.RED +"Command not recognized by the logic...")
 
 def main():
-    clear_terminal()
-    user_query = input(f"{bot}Enter your search query: ")
+    global audio_playing
+    song_name = input(Fore.BLUE + "Music Name: ")
+    print(Fore.GREEN + "\nSearching...")
+    # create a VLC media player instance with high-quality audio output
+    instance = vlc.Instance("--aout=waveout")
 
-    # Check for a number between 200 and 300 in the query
-    volume_level = 0.5  # Default volume
+    # create a VLC media player
+    player = instance.media_player_new()
 
-    audio_url = get_audio_url(user_query)
-    if audio_url:
-        print(f"{bot}Searching for '{user_query}'")
+    # Use subprocess to call yt-dlp with the --verbose flag and specify high-quality audio
+    youtube_url = None
+    try:
+        process = subprocess.Popen(
+            [
+                'yt-dlp',
+                '--verbose',
+                '--skip-download',
+                '--format', 'bestaudio/best',
+                '--get-url',
+                f'ytsearch:{song_name}'
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            youtube_url = stdout.strip()
+        else:
+            print(Fore.RED + "Error:", stderr)
+            sys.exit(1)
+    except FileNotFoundError:
+        print("Error: yt-dlp not found. Please install yt-dlp.")
+        sys.exit(1)
 
-        looping = False
-        controller = None
-        is_playing = False
+    if youtube_url:
+        # create a VLC Media object and set it to the audio URL
+        media = instance.media_new(youtube_url)
+        player.set_media(media)
 
-        while True:
-            print(f"{bot}Playing from '{user_query}' in search")
+        print(Fore.BLUE + f"\nPlaying from '{song_name}' in search")
+        audio_playing = True
+        # create separate threads for playback and command input
+        playback_thread_instance = threading.Thread(target=playback_thread, args=(player, youtube_url))
+        command_thread_instance = threading.Thread(target=command_thread, args=(player,))
 
-            if not is_playing and controller is None:
-                controller = AudioController(audio_url, volume_level)
-                controller.play()
-                is_playing = True
+        # sart both threads
+        playback_thread_instance.start()
+        command_thread_instance.start()
 
-            if looping and not is_playing:
-                controller.play()  # Restart playback for looping
-                is_playing = True
-
-            command = input(f"{bot}Enter 'loop' to toggle loop, 'change' to play a new song, or 'stop' to stop: ")
-            if command == 'loop':
-                looping = not looping
-                if looping:
-                    print("Looping enabled")
-                else:
-                    print("Looping disabled")
-            elif command == 'change':
-                looping = False
-                controller.stop()  # Stop the current playback
-                controller = None  # Reset the controller
-                is_playing = False  # Reset playback status
-                user_query = input(f"{bot}Enter a new search query: ")
-                audio_url = get_audio_url(user_query)
-                if audio_url:
-                    print(f"{bot}Searching for '{user_query}'")
-            elif command == 'stop':
-                looping = False
-                if controller:
-                    controller.stop()  # Stop the current playback
-                controller = None  # Reset the controller
-                is_playing = False  # Reset playback status
-                break
-            else:
-                print(f"{bot}Invalid command. Please enter 'loop', 'change', or 'stop'.")
-
-            time.sleep(1)
+        # wait for both threads to finish
+        playback_thread_instance.join()
+        command_thread_instance.join()
 
 if __name__ == "__main__":
     main()
